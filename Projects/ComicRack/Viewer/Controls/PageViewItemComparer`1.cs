207108f83d88e00688b4ada9a08749f1d5a3cfe7
ChrisMathews393 <christophermathews393@gmail.com>
2023-04-04 23:30:34 -0600

    Initial commit

---
+++ Projects/ComicRack/Viewer/Controls/PageViewItemComparer`1.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: cYo.Projects.ComicRack.Viewer.Controls.PageViewItemComparer`1
+// Assembly: ComicRack, Version=1.0.5915.38777, Culture=neutral, PublicKeyToken=b3ca110c99b4b731
+// MVID: E9032406-66BB-46BB-A802-D697DB44DA19
+// Assembly location: C:\Program Files\ComicRack\ComicRack.exe
+
+using cYo.Common.Windows.Forms;
+using cYo.Projects.ComicRack.Engine;
+using System.Collections.Generic;
+
+namespace cYo.Projects.ComicRack.Viewer.Controls
+{
+  public class PageViewItemComparer<T> : Comparer<PageViewItem>, IComparer<IViewableItem> where T : IComparer<ComicPageInfo>, new()
+  {
+    private readonly T comparer = new T();
+
+    int IComparer<IViewableItem>.Compare(IViewableItem x, IViewableItem y) => this.Compare((PageViewItem) x, (PageViewItem) y);
+
+    public override int Compare(PageViewItem x, PageViewItem y) => this.comparer.Compare(x.Book.Comic.GetPage(x.Page), y.Book.Comic.GetPage(y.Page));
+  }
+}
