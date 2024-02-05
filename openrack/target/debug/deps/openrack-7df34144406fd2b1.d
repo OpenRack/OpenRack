cb2e2e75892c3b0b596f8f5dedf9b7c5af870447
0xChloe <chloe@catastrophesoftware.com>
2024-02-05 06:17:36 +0000

    deleted all python stuff

---
+++ openrack/target/debug/deps/openrack-7df34144406fd2b1.d
@@ -0,0 +1,5 @@
+/home/chloe/repos/OpenRack/openrack/target/debug/deps/openrack-7df34144406fd2b1.rmeta: src/main.rs
+
+/home/chloe/repos/OpenRack/openrack/target/debug/deps/openrack-7df34144406fd2b1.d: src/main.rs
+
+src/main.rs:
---
+++ openrack/target/debug/deps/openrack-a94916a0f4d8bbba.d
@@ -0,0 +1,5 @@
+/home/chloe/repos/OpenRack/openrack/target/debug/deps/openrack-a94916a0f4d8bbba.rmeta: src/main.rs
+
+/home/chloe/repos/OpenRack/openrack/target/debug/deps/openrack-a94916a0f4d8bbba.d: src/main.rs
+
+src/main.rs:
